9b9a6e45494e8c7d6b4d3184758b27d11eb5a432
Icoff9077 <ianjcoff@gmail.com>
2026-04-22 20:13:21 -0700

    Major Stats

--- src/major-stats.cpp
+++ src/major-stats.cpp
@@ -15,10 +15,10 @@ Fighter compsci(const std::string& playerName) {
 
     //the numbers at the end are attack power,
     //accuracy and critical hit chance values, for example
-    //18,90,15 would be attack power 18 with 90 percent accuracy and 15 percent crit chance.
-    f.moves[0] = {"Physical Attack", MoveKind::Attack, 18, 90, 15};
-    f.moves[1] = {"Magical Attack", MoveKind::Attack, 14, 95, 10};
-    f.moves[2] = {"Block", MoveKind::Defend, 6, 95, 0};
+    //18,15 would be attack power 18 with 15 percent crit chance.
+    f.moves[0] = {"Physical Attack", MoveKind::Attack, 18, 15};
+    f.moves[1] = {"Magical Attack", MoveKind::Attack, 14, 10};
+    f.moves[2] = {"Block", MoveKind::Defend, 6, 0};
 
     return f;
 
